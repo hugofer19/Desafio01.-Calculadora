cab2a72c4462b5970be4b1785147c2db3da557aa
Hugo Ferreira <46359390+hugofer19@users.noreply.github.com>
2024-05-16 00:39:18 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+#pedir dois numeros
+num1 = int(input("Digite o número 1: "))
+num2 = int(input("Digite o número 2: "))
+
+#operação
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+#mostra soma, subtração, multiplicação e divisão desses numeros
+
+print("Soma = ", soma)
+print("Subtração = ", subtracao)
+print("Multiplicação = ", multiplicacao)
+print("Divisão = ", divisao)
